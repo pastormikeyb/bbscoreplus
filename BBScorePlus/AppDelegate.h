132ee0a89b8c49fdea458548c8d0955fdad849fa
Michael Usry <mbudesigns@me.com>
2015-10-11 21:16:24 -0400

    Initial Commit

---
+++ BBScorePlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBScorePlus
+//
+//  Created by Michael on 10/11/15.
+//  Copyright © 2015 Michael Usry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBScorePlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBScorePlus
+//
+//  Created by Michael on 10/11/15.
+//  Copyright © 2015 Michael Usry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
